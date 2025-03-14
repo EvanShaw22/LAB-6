baef7ce08f069829a9ab108d9183849e8d0c6e7d
EvanShaw22 <g00423991@atu.ie>
2025-03-14 13:45:51 +0000

    FIRST COMMIT

---
+++ src/app/connacht/connacht.page.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { IonButtons, IonBackButton, IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+
+@Component({
+  selector: 'app-connacht',
+  templateUrl: './connacht.page.html',
+  styleUrls: ['./connacht.page.scss'],
+  standalone: true,
+  imports: [IonButtons, IonBackButton, IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+})
+export class ConnachtPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
